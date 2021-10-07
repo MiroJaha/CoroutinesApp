94536a72baf254aa857186af2805453cbd2a3f63
Mohammad Jaha <miro.tune@gmail.com>
2021-10-07 10:55:07 +0300

    updates

--- app/src/main/java/com/example/coroutinesapp/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesapp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.coroutinesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
@@ -13,10 +15,14 @@ import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var tView:TextView
+    private lateinit var tView: TextView
     private lateinit var button: Button
+    private lateinit var button2: Button
 
     val adviceUrl = "https://api.adviceslip.com/advice"
+    var response = ""
+    var advice = ""
+    var check = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,31 +30,46 @@ class MainActivity : AppCompatActivity() {
 
         tView = findViewById(R.id.tvMain)
         button = findViewById(R.id.button)
+        button2 = findViewById(R.id.button2)
 
-        var response = ""
+        val handler = Handler()
 
         button.setOnClickListener {
+            reDo()
+            if(check)
+                handler.postDelayed({
+                    button.performClick()
+                }, 3000)
+            check = true
+        }
 
-            CoroutineScope(IO).launch {
-                try {
-                    response = URL(adviceUrl).readText(Charsets.UTF_8)
+        button2.setOnClickListener {
+            check = false
+            handler.postDelayed({
+                Toast.makeText(this,"onPause",Toast.LENGTH_LONG).show()
+            }, 3000)
+        }
 
-                } catch (e: Exception) {
-                }
+    }
 
-                var data = async { response }.await()
+    private fun reDo() {
+        CoroutineScope(IO).launch {
+            try {
+                response = URL(adviceUrl).readText(Charsets.UTF_8)
 
-                if (data.isNotEmpty()) {
-                    withContext(Main) {
-                        val jsonObject = JSONObject(data)
-                        val slip = jsonObject.getJSONObject("slip")
-                        val advice = slip.getString("advice")
-
-                        tView.text = advice
-                    }
-                }
+            } catch (e: Exception) {
             }
 
+            var data = async { response }.await()
+            if (data.isNotEmpty()) {
+                withContext(Main) {
+                    val jsonObject = JSONObject(data)
+                    val slip = jsonObject.getJSONObject("slip")
+                    advice = slip.getString("advice")
+                    tView.text = advice
+                }
+            }
         }
     }
+
 }
